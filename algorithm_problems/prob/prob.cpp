b27b63f4bb6997dbb0290eec8abc1af502f987e3
EL3FT <pwnable@pm.me>
2019-02-20 17:40:36 +0900

    commit

---
+++ algorithm_problems/prob/prob.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+
+    return answer;
+}
+
+int main() {
+    vector<int> priorities;
+    int line;
+    int location;
+
+    
+    cin >> location;
+
+    cout << solution(priorities, location) << endl;
+
+    return 0;
+}
